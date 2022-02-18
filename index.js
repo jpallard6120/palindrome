ba66e7ec831662cab6882350ac89cab16e9b0946
Jean-Philippe Allard <jp@jpallard.com>
2022-02-18 13:44:22 -0500

    Added readme

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
-function reverse(string) {
-    return Array.from(string).reverse().join("");
-}
+module.exports = Phrase;
 
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === reverse(processedContent);
-}
-
-function Phrase(content) {
-    this.content = content;
-
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-
-    this.processedContent = function processedContent() {
-        return this.processor(this.content);
-    };
-
-    this.palindrome = function palindrome() {
-        return this.processedContent() === reverse(this.processedContent());
-    };
-};
-
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Override the processedContent method from the Phrase object
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
-    }
-}
-
-TranslatedPhrase.prototype = new Phrase();
-
-function emailParts(email) {
-    username = email.split("@")[0]
-    remaining = email.split("@")[1]
-    domain = remaining.split(".")[0]
-    tld = remaining.split(".")[1]
-    return email_array = [username, domain, tld]
-}
-
-// Monkey Patching the native String object
+// Adds `reverse` to all strings.
 String.prototype.reverse = function() {
     return Array.from(this).reverse().join("");
 }
 
+// Defines a Phrase object.
 function Phrase(content) {
     this.content = content;
 
-    this.processor = function(string) {
-        return string.toLowerCase();
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
     }
 
-    this.processedContent = function processedContent() {
-        return this.processor(this.content);
-    };
-
+    // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome() {
         return this.processedContent() === this.processedContent().reverse();
-    };
-};
+    }
+}
